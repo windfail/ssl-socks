2a986d00c9e54d8a604ccea5097421a2c0700daa
GriffOn <yily_101@163.com>
2021-12-08 23:50:04 +0800

    debug/test change

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,8 @@
-for f in udp_server udp_client tcp_server tcp_client;do
-    echo compile $f
-    g++ $f.cpp -lpthread -o $f
-done
+#for f in udp_server udp_client tcp_server tcp_client;do
+    #echo compile $f
+    #g++ $f.cpp -lpthread -o $f
+#done
 
-ifconfig eth0:0 10.10.10.20
 ip rule add fwmark 1 table 100
 ip route add local default table 100 dev lo
 nft -f nfttest.conf
@@ -25,7 +24,6 @@ for j in $(jobs |sed -n 's/\[\([0-9]\)\].*/\1/p') ;do
     kill %$j
 done
 
-ifconfig eth0:0 down
 nft flush ruleset
 ip rule del fwmark 1
 ip route del local default table 100 dev lo
